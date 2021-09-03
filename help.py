f0cfcbeab3c5e73bc3a92524ce5d32370aff0072
Raghulal Prajapati <56857544+prajapatiraghulal@users.noreply.github.com>
2021-09-03 07:51:22 +0530

    Add files via upload
    This is the updated version of my editor with functionality of next word detection.

--- help.py
+++ help.py
@@ -8,8 +8,9 @@ text.insert(END, "This is a phrase.\n")
 text.insert(END, "Bye bye...")
 text.pack(expand=1, fill=BOTH)
 
-# adding a tag to a part of text specifying the indices
-text.tag_add("start", "2.1", "2.13")
-text.tag_config("start", background="black", foreground="yellow")
+print(help(text.replace))
+# # adding a tag to a part of text specifying the indices
+# text.tag_add("start", "2.1", "2.13")
+# text.tag_config("start", background="black", foreground="yellow")
 
 root.mainloop()
